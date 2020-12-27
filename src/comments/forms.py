c007c74688fd71e7f1fa85bfd758b0a191673137
Abhishek Sahu <abhisheksahu92@outlook.com>
2020-12-27 18:56:54 +0530

    Created Reply option on Comment.

--- src/comments/forms.py
+++ src/comments/forms.py
@@ -4,4 +4,5 @@ from django.forms.widgets import HiddenInput, Textarea
 class CommentForm(forms.Form):
     content_type = forms.CharField(widget=HiddenInput)
     object_id    = forms.IntegerField(widget=HiddenInput)
+    #parent_id    = forms.IntegerField(widget=HiddenInput)
     content      = forms.CharField(widget=Textarea)
